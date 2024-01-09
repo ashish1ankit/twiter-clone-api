f4b23ee644ad553a16b642eaeee4f145326228f0
ashish1ankit <ashish.ankit@yahoo.com>
2024-01-08 21:52:23 -0800

    Initial code commit

---
+++ twiter-clone-api/src/main/java/com/clone/com/twiter_clone_api/App.java
@@ -0,0 +1,13 @@
+package com.clone.com.twiter_clone_api;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
