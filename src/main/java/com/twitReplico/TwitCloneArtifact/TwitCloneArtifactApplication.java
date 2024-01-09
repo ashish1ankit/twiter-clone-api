fbc9801f79245dda6d020a0d3ea4715746d11a04
ashish1ankit <ashish.ankit@yahoo.com>
2024-01-09 00:11:41 -0800

    Updated code.

---
+++ src/main/java/com/twitReplico/TwitCloneArtifact/TwitCloneArtifactApplication.java
@@ -0,0 +1,13 @@
+package com.twitReplico.TwitCloneArtifact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitCloneArtifactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitCloneArtifactApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/twitReplico/TwitCloneArtifact/TwitCloneArtifactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twitReplico.TwitCloneArtifact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitCloneArtifactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
